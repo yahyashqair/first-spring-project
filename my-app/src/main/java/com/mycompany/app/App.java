be339d070d323fccdcbf95a907296ed450e7c366
yahya shqair <y.shqair@hotmail.com>
2019-06-19 08:57:01 +0300

    add class person

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App
 {
     public static void main( String[] args )
     {
+		SpringApplication.run(App.class, args);
         System.out.println( "Hello World!" );
     }
 }
---
+++ my-app/src/main/java/com/mycompany/app/person.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+import org.springframework.beans.factory.InitializingBean;
+
+public class person implements InitializingBean {
+	String x ;
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		
+	} 
+	
+}
